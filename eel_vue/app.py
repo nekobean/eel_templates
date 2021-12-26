3f92fd56e0fa53557f194a47b81ea7752799fb72
nekobean <nobepon@gmail.com>
2021-12-27 01:18:09 +0900

    update app

--- eel_vue/app.py
+++ eel_vue/app.py
@@ -1,16 +1,10 @@
 import eel
 
-# Set web files folder
-eel.init("web")
+
+def main():
+    eel.init("web")
+    eel.start("index.html", size=(1024, 768))
 
 
-@eel.expose  # Expose this function to Javascript
-def say_hello_py(x):
-    print(f"Hello from {x}")
-
-
-say_hello_py("Python World!")
-eel.say_hello_js("Python World!")  # Call a Javascript function
-
-# Start application
-eel.start("index.html", size=(640, 480))
+if __name__ == "__main__":
+    main()
